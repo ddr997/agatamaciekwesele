bf1e1394e3192862a996a9653fb183fa018cc09f
MacLuk <ziomek23pl@hotmail.com>
2026-08-07 22:12:02 +0200

    project cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ddr997.github.io",
 	base: '/agatamaciekwesele',
 	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
 });
