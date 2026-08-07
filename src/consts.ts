765835ef550b2b192ac125e3171212cea9df372c
MacLuk <ziomek23pl@hotmail.com>
2026-08-07 21:56:55 +0200

    kkk

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Agata i Maciek - Ślub";
+export const SITE_DESCRIPTION = "Strona naszego wesela - 28.08.2026";
